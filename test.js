c83f0ce110652310eec7a726e6a09c0e409d8962
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-12 18:18:28 +0100

    removed unnecessary files

--- test.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello Node!');
-console.log('Goodbye');
-console.log('I edited this vile file with vim');
-let PI = 3.14159265359;
-module.exports.area = (radius) => Math.pow(radius, 2) * PI;
-module.exports.circum = (radius) => 2 * radius * PI;
-
--- test2.js
+++
@@ -1,4 +0,0 @@
-const circle = require('./test.js');
-let r = 3;
-console.log(`Circle with radius ${r} has area: ${circle.area(r)} and circumference: ${circle.circum(r)}`);
-
