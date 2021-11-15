64adc56881129815c06de2a1691ed21314547755
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-15 10:12:28 +0100

    degugged and prettified

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
-const express = require("express");
-const app = express();
+const express = require("express"); const app = express();
 
-let favMovies = [
-  {
-    title: 'the Immigrant',
-    director: 'Charlie Chaplin'
-  },
-  {
-    title: 'M',
-    director: 'Fritz Lang'
-  },
-  {
-    title: 'the Big Labowski',
-    director: 'the Cohen Brothers'
-  }
-]
+let favMovies = [ { title: 'the Immigrant', director: 'Charlie Chaplin' }, {
+  title: 'M', director: 'Fritz Lang' }, { title: 'the Big Labowski', director:
+    'the Cohen Brothers' } ];
 
 // GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to myFlix');
-});
+app.get('/', (req, res) => { res.send('Welcome to myFlix'); });
 
 app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
-});
+  res.sendFile('public/documentation.html', { root: __dirname }); });
 
-app.get('/movies', (req, res) => {
-  res.json(favMovies)
+app.get('/movies', (req, res) => { res.json(favMovies) });
 
 // listen for requests
-app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.);
+app.listen(8080, () => { console.log('Your app is listening on port 8080.');
 });
-
-
 
