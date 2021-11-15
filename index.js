0b0fc58806d9a6df045bde1c8903954f22545d9e
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-15 11:57:15 +0100

    added static method, renamed documentation to doc.html

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-const express = require("express");
+const express = require('express');
+const morgan = require('morgan');
+
 const app = express();
 
 let favMovies = [
-  { title: "the Immigrant", director: "Charlie Chaplin" },
+  { title: 'the Immigrant', director: 'Charlie Chaplin' },
   {
-    title: "M",
-    director: "Fritz Lang",
+    title: 'M',
+    director: 'Fritz Lang',
   },
-  { title: "the Big Labowski", director: "the Cohen Brothers" },
+  { title: 'the Big Labowski', director: 'the Cohen Brothers' },
 ];
-
-// write middleware functions
-//
-// here is url logging function, it logs the URL request
-const myLogger = (req, res, next) => {
-  console.log(req.url);
-  next();
-};
 
 // send back the timestamp of the request
 const requestTime = (req, res, next) => {
@@ -25,24 +19,24 @@ const requestTime = (req, res, next) => {
 };
 
 // tell the app to use the middleware funtions for all requests
-app.use(myLogger);
+
+app.use(morgan('common'));
 app.use(requestTime);
+app.use(express.static('./public'));
+
 
 // GET requests
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.send(`Welcome to myFlix.
    \n\n <small> You accessed this site at ${req.requestTime}</small>`);
 });
 
-app.get("/documentation", (req, res) => {
-  res.sendFile("public/documentation.html", { root: __dirname });
-});
 
-app.get("/movies", (req, res) => {
+app.get('/movies', (req, res) => {
   res.json(favMovies);
 });
 
 // listen for requests
 app.listen(8080, () => {
-  console.log("Your app is listening on port 8080.");
+  console.log('Your app is listening on port 8080.');
 });
