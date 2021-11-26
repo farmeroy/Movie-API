a26aa7b19acfc6efd32d9302c1e8a9874e6954f3
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-11-26 10:58:05 +0100

    hashSync() -> async await hash()

--- models.js
+++ models.js
@@ -27,8 +27,8 @@ let userSchema = mongoose.Schema({
 });
 
 // this method hashses a new user's password
-userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
+userSchema.statics.hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
 };
 
 // this method hashes a password on login to compare to the stored hashed password
