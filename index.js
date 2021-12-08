ce0de634d266c5f3bd33450ea6cd916759e80955
Raffaele Cataldo <kemanicataldo@gmail.com>
2021-12-08 10:49:03 +0100

    added localhost to allowedOrigins

--- index.js
+++ index.js
@@ -63,24 +63,24 @@ app.use(passport.initialize());
 
 // install and use CORS
 const cors = require('cors');
-app.use(cors());
-// define our CORS allowed origins
-// const allowedOrigins = ['https://localhost:3000', 'https://testsite.com'];
-// // call our CORS policy and check for origins
-// app.use(
-//   cors({
-//     origin: (origin, callback) => {
-//       if (!origin) return callback(null, true);
-//       if (allowedOrigins.indexOf(origin) === -1) {
-//         const message =
-//           'The CORS policy for this application does not allow requests from origin ' +
-//           origin;
-//         return callback(new Error(message), false);
-//       }
-//       return callback(null, true);
-//     },
-//   })
-// );
+// app.use(cors());
+//define our CORS allowed origins
+const allowedOrigins = ['https://localhost:1234', 'https://testsite.com'];
+// call our CORS policy and check for origins
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const message =
+          'The CORS policy for this application does not allow requests from origin ' +
+          origin;
+        return callback(new Error(message), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 
 
