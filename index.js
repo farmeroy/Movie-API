9f743d5ba5a820fa53ee74d7c0d91b08ed269a8a
Raffaele Cataldo <kemanicataldo@gmail.com>
2022-02-15 11:48:07 +0100

    allow all origins

--- index.js
+++ index.js
@@ -65,25 +65,25 @@ app.use(passport.initialize());
 const cors = require('cors');
 //
 // allow all origins
-// app.use(cors());
+app.use(cors());
 //
 //define our CORS allowed origins
 const allowedOrigins = ['http://localhost:4200', 'https://testsite.com', 'https://pre-code-flix.netlify.app'];
 // call our CORS policy and check for origins
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.indexOf(origin) === -1) {
-        const message =
-          'The CORS policy for this application does not allow requests from origin ' +
-          origin;
-        return callback(new Error(message), false);
-      }
-      return callback(null, true);
-    },
-  })
-);
+// app.use(
+//   cors({
+//     origin: (origin, callback) => {
+//       if (!origin) return callback(null, true);
+//       if (allowedOrigins.indexOf(origin) === -1) {
+//         const message =
+//           'The CORS policy for this application does not allow requests from origin ' +
+//           origin;
+//         return callback(new Error(message), false);
+//       }
+//       return callback(null, true);
+//     },
+//   })
+// );
 
 
 
